eb30817135334f80156e776c3f3d3083cd462738
Nick Sutterer <apotonick@gmail.com>
2017-04-14 15:35:28 +0200

    better files.

---
+++ lib/trailblazer/diagram/bpmn.rb
@@ -0,0 +1,83 @@
+require "representable"
+require "representable/xml"
+
+module Trailblazer
+  module Diagram
+    module BPMN
+      Definitions = Struct.new(:process, :diagram)
+
+      # Representers for BPMN XML.
+      module Representer
+        class Task < Representable::Decorator
+          include Representable::XML
+          include Representable::XML::Namespace
+          # namespace "http://www.omg.org/spec/BPMN/20100524/MODEL"
+
+          self.representation_wrap = :task # overridden via :as.
+
+          property :id,   attribute: true
+          property :name, attribute: true
+
+          collection :outgoing, exec_context: :decorator
+          collection :incoming, exec_context: :decorator
+
+          def outgoing
+            represented.outgoing.map(&:id)
+          end
+
+          def incoming
+            represented.incoming.map(&:id)
+          end
+        end
+
+        class SequenceFlow < Representable::Decorator
+          include Representable::XML
+          self.representation_wrap = :SequenceFlow
+
+          property :id,   attribute: true
+          property :sourceRef, attribute: true, exec_context: :decorator
+          property :targetRef, attribute: true, exec_context: :decorator
+
+          def sourceRef
+            represented.sourceRef.id
+          end
+
+          def targetRef
+            represented.targetRef.id
+          end
+        end
+
+        class Process < Representable::Decorator
+          include Representable::XML
+          include Representable::XML::Namespace
+          self.representation_wrap = :process
+
+          namespace "http://www.omg.org/spec/BPMN/20100524/MODEL"
+
+          collection :start_events, as: :startEvent, decorator: Task, namespace: "bpmn"
+          collection :end_events, as: :endEvent, decorator: Task,     namespace: "bpmn"
+          collection :task, decorator: Task,                          namespace: "bpmn"
+          collection :sequence_flow, decorator: SequenceFlow, as: :sequenceFlow
+        end
+
+        class Diagram < Representable::Decorator
+          include Representable::XML
+          include Representable::XML::Namespace
+          self.representation_wrap = :diagram
+
+          namespace "http://www.omg.org/spec/BPMN/20100524/DI"
+        end
+
+        class Definitions < Representable::Decorator
+          include Representable::XML
+          include Representable::XML::Namespace
+          self.representation_wrap = :"bpmn:definitions" # TODO: how to add a "self" namespace as if someone called to_xml(namespace: :bmpn) on us?
+
+          property :process, decorator: Process, namespace: "bpmn"
+          property :diagram, decorator: Diagram, namespace: "bpmndi"
+        end
+      end
+    end
+  end
+end
+
---
+++ test/diagram_test.rb
@@ -0,0 +1,50 @@
+require "test_helper"
+
+
+require "trailblazer/developer"
+require "trailblazer/diagram/bpmn"
+require "trailblazer/circuit"
+
+# puts Event::Start.new(start).to_xml
+
+class DiagramXMLTest < Minitest::Spec
+  Circuit = Trailblazer::Circuit
+
+  module Blog
+    Read    = ->(*) { snippet }
+    Next    = ->(*) { snippet }
+    Comment = ->(*) { snippet }
+  end
+
+  let(:blog) do
+    Circuit::Activity(id: "blog.read/next", Blog::Read=>:Read, Blog::Next=>:Next, Blog::Comment=>:Comment) { |evt|
+      {
+        evt[:Start]  => { Circuit::Right => Blog::Read },
+        Blog::Read => { Circuit::Right => Blog::Next },
+        Blog::Next => { Circuit::Right => evt[:End], Circuit::Left => Blog::Comment },
+        Blog::Comment => { Circuit::Right => evt[:End] }
+      }
+    }
+  end
+
+  it do
+    require "trailblazer/developer/circuit"
+    model = Trailblazer::Developer::Circuit.bla(blog)
+
+    # raise model.task[0].inspect
+
+    puts Trailblazer::Diagram::BPMN::Representer::Definitions.new(Trailblazer::Diagram::BPMN::Definitions.new(model)).to_xml
+  end
+end
+
+#     <bpmn:startEvent id="StartEvent_1">
+#       <bpmn:outgoing>SequenceFlow_1wsxfd0</bpmn:outgoing>
+#     </bpmn:startEvent>
+#     <bpmn:task id="Task_0vhpnru" name="write&#10;">
+#       <bpmn:incoming>SequenceFlow_1wsxfd0</bpmn:incoming>
+#       <bpmn:outgoing>SequenceFlow_0iwxv4o</bpmn:outgoing>
+#     </bpmn:task>
+#     <bpmn:sequenceFlow id="SequenceFlow_1wsxfd0" sourceRef="StartEvent_1" targetRef="Task_0vhpnru" />
+# <bpmn:endEvent id="EndEvent_03m4k14">
+#       <bpmn:incoming>SequenceFlow_1cisj8a</bpmn:incoming>
+#     </bpmn:endEvent>
