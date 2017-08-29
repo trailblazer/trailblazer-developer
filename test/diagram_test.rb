5aaf764841f81828217867f38d3b192acaa3a63e
Nick Sutterer <apotonick@gmail.com>
2017-08-29 22:30:20 +0200

    fix test.

--- test/diagram_test.rb
+++ test/diagram_test.rb
@@ -61,21 +61,21 @@ class DiagramXMLTest < Minitest::Spec
 
     File.write("berry.bpmn", xml)
 
-    token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJub25lIn0.eyJpZCI6NiwidXNlcm5hbWUiOiJkdWJlbCIsImVtYWlsIjoiZHViZWxAZHViZWwuZHViZWwifQ."
-    token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJub25lIn0.eyJpZCI6MywidXNlcm5hbWUiOiJhcG90b25pY2siLCJlbWFpbCI6Im5pY2tAdHJhaWxibGF6ZXIudG8ifQ."
+    # token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJub25lIn0.eyJpZCI6NiwidXNlcm5hbWUiOiJkdWJlbCIsImVtYWlsIjoiZHViZWxAZHViZWwuZHViZWwifQ."
+    # token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJub25lIn0.eyJpZCI6MywidXNlcm5hbWUiOiJhcG90b25pY2siLCJlbWFpbCI6Im5pY2tAdHJhaWxibGF6ZXIudG8ifQ."
 
-    require "faraday"
-    conn = Faraday.new(:url => 'http://localhost:3477')
-    response = conn.post do |req|
-      req.url '/dev/v1/import'
-      req.headers['Content-Type'] = 'application/json'
-      req.headers["Authorization"] = token
-      require "base64"
+    # require "faraday"
+    # conn = Faraday.new(:url => 'http://localhost:3477')
+    # response = conn.post do |req|
+    #   req.url '/dev/v1/import'
+    #   req.headers['Content-Type'] = 'application/json'
+    #   req.headers["Authorization"] = token
+    #   require "base64"
 
-      req.body = %{{ "name": "Unagi", "xml":"#{Base64.strict_encode64(xml)}" }}
-    end
+    #   req.body = %{{ "name": "Unagi", "xml":"#{Base64.strict_encode64(xml)}" }}
+    # end
 
-    puts response.status.inspect
+    # puts response.status.inspect
 
     xml.must_equal File.read(File.dirname(__FILE__) + "/xml/operation.bpmn").chomp
   end
