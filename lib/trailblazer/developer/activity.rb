a23eba647327ed0ae121bf5a01a0ae83f27daa38
Nick Sutterer <apotonick@gmail.com>
2017-08-30 08:56:59 +0200

    move it into Graph because that's what we do.

--- lib/trailblazer/developer/activity.rb
+++ lib/trailblazer/developer/activity.rb
@@ -1,5 +1,3 @@
-require 'securerandom'
-
 module Trailblazer
   module Developer
     # Transforms an {Activity::Graph} into an abstract data structure that represents the graph via a
@@ -8,27 +6,29 @@ module Trailblazer
     #
     # It returns {Model} with {Task}s and {Flow}s.
     module Activity
-      Model = Struct.new(:id, :start_events, :end_events, :task, :sequence_flow)
-      Task  = Struct.new(:id, :name, :options, :outgoing, :incoming)
-      Flow  = Struct.new(:id, :sourceRef, :targetRef, :direction) # DISCUSS: direction ATM is the "condition" for the BPMN rendering.
+      module Graph
+        Model = Struct.new(:id, :start_events, :end_events, :task, :sequence_flow)
+        Task  = Struct.new(:id, :name, :options, :outgoing, :incoming)
+        Flow  = Struct.new(:id, :sourceRef, :targetRef, :direction) # DISCUSS: direction ATM is the "condition" for the BPMN rendering.
 
-      # @param Graph an object implementing the Activity::Graph interface
-      # @return Model Generic representation of the graph, ready for rendering.
-      def self.to_model(graph, id: "some-process")
-        start_events = graph.find_all("Start.default") # FIXME. this is a static assumption.
-        end_events   = graph.find_all { |node| graph.successors(node).size == 0 }
-        tasks        = graph.find_all { |node| true }
-        tasks       -= start_events
-        tasks       -= end_events
-        edges        = graph.find_all { |node| true }.collect { |node| graph.successors(node).collect { |node, edge| edge } }.flatten(1)
+        # @param Graph an object implementing the Activity::Graph interface
+        # @return Model Generic representation of the graph, ready for rendering.
+        def self.to_model(graph, id: "some-process")
+          start_events = graph.find_all("Start.default") # FIXME. this is a static assumption.
+          end_events   = graph.find_all { |node| graph.successors(node).size == 0 }
+          tasks        = graph.find_all { |node| true }
+          tasks       -= start_events
+          tasks       -= end_events
+          edges        = graph.find_all { |node| true }.collect { |node| graph.successors(node).collect { |node, edge| edge } }.flatten(1)
 
-        # transform nodes into BPMN elements.
-        start_events = start_events.collect { |evt| Task.new( evt[:id], evt[:id], evt, graph.successors(evt).collect(&:last), graph.predecessors(evt).collect(&:last) ) }
-        end_events   =   end_events.collect { |evt| Task.new( evt[:id], evt[:id], evt, graph.successors(evt).collect(&:last), graph.predecessors(evt).collect(&:last) ) }
-        tasks        =        tasks.collect { |evt| Task.new( evt[:id], evt[:id], evt, graph.successors(evt).collect(&:last), graph.predecessors(evt).collect(&:last) ) }
-        edges        = edges.collect { |edge| Flow.new( edge[:id], edge[:source][:id], edge[:target][:id], edge[:_wrapped] ) }
+          # transform nodes into BPMN elements.
+          start_events = start_events.collect { |evt| Task.new( evt[:id], evt[:id], evt, graph.successors(evt).collect(&:last), graph.predecessors(evt).collect(&:last) ) }
+          end_events   =   end_events.collect { |evt| Task.new( evt[:id], evt[:id], evt, graph.successors(evt).collect(&:last), graph.predecessors(evt).collect(&:last) ) }
+          tasks        =        tasks.collect { |evt| Task.new( evt[:id], evt[:id], evt, graph.successors(evt).collect(&:last), graph.predecessors(evt).collect(&:last) ) }
+          edges        = edges.collect { |edge| Flow.new( edge[:id], edge[:source][:id], edge[:target][:id], edge[:_wrapped] ) }
 
-        Model.new(id, start_events, end_events, tasks, edges)
+          Model.new(id, start_events, end_events, tasks, edges)
+        end
       end
     end
   end
