9f964f03f69f90a795d711aeea9c58ee99068624
Nick Sutterer <apotonick@gmail.com>
2019-03-28 19:22:58 +0100

    fix render_test.

--- test/render_test.rb
+++ test/render_test.rb
@@ -4,23 +4,20 @@ require "trailblazer/developer/render/circuit"
 
 class RenderCircuitTest < Minitest::Spec
   it "renders" do
-    activity = Module.new do
-      extend Trailblazer::Activity::Railway()
-      # extend T.def_steps(:a, :b)
-
-      step :a, Output(:failure) => "End.success"
+    activity = Class.new(Trailblazer::Activity::Railway) do
+      step :a, Output(:failure) => Id("End.success")
       step :b
     end
 
-    Trailblazer::Developer::Render::Circuit.(activity).must_equal %{
+    Trailblazer::Developer::Render::Circuit.(activity.to_h).must_equal %{
 #<Start/:default>
  {Trailblazer::Activity::Right} => #<Trailblazer::Activity::TaskBuilder::Task user_proc=a>
 #<Trailblazer::Activity::TaskBuilder::Task user_proc=a>
- {Trailblazer::Activity::Right} => #<Trailblazer::Activity::TaskBuilder::Task user_proc=b>
  {Trailblazer::Activity::Left} => #<End/:success>
+ {Trailblazer::Activity::Right} => #<Trailblazer::Activity::TaskBuilder::Task user_proc=b>
 #<Trailblazer::Activity::TaskBuilder::Task user_proc=b>
- {Trailblazer::Activity::Right} => #<End/:success>
  {Trailblazer::Activity::Left} => #<End/:failure>
+ {Trailblazer::Activity::Right} => #<End/:success>
 #<End/:success>
 
 #<End/:failure>
