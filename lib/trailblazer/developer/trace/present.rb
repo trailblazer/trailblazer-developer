bec668cad194afc2ecccc3850a7b5967acae4223
Nick Sutterer <apotonick@gmail.com>
2022-10-22 18:00:46 +0200

    always use the :id in Trace::Present, no exceptions.

--- lib/trailblazer/developer/trace/present.rb
+++ lib/trailblazer/developer/trace/present.rb
@@ -1,26 +1,28 @@
-require 'hirb'
+require "hirb"
 
 module Trailblazer::Developer
   module Trace
     module Present
       module_function
 
+      # @private
       def default_renderer(node:, **)
-        task      = node.captured_input[:task]
-        activity  = node.captured_input[:activity]
+        task        = node.captured_input[:task]
+        activity    = node.captured_input[:activity]
 
-        graph      = Trailblazer::Activity::Introspect::Graph(activity) # TODO: cache for run.
-        graph_node = graph.find { |n| n[:task] == task }
+        graph       = Trailblazer::Activity::Introspect::Graph(activity) # TODO: cache for run.
+        graph_node  = graph.find { |n| n[:task] == task }
 
-        name  = graph_node ? graph_node[:id] : task
+        name        = graph_node[:id]
 
         [node.level, name]
       end
 
-      def call(stack, level: 1, tree: [], renderer: method(:default_renderer), **options)
-        tree, processed = Dev::Trace.Tree(stack.to_a) # TODO: those lines need to be extracted
-        # parent_map = Dev::Trace::Tree::ParentMap.for(tree)
-        enumerable_tree = Dev::Trace::Tree.Enumerable(tree)
+      # Entry point for rendering a Stack as a "tree branch" the way we do it in {#wtf?}.
+      def call(stack, level: 1, tree: [], renderer: method(:default_renderer), **)
+        tree, processed = Trace.Tree(stack.to_a) # TODO: those lines need to be extracted
+        # parent_map = Trace::Tree::ParentMap.for(tree)
+        enumerable_tree = Trace::Tree.Enumerable(tree)
 
         nodes = enumerable_tree.each_with_index.collect do |node, position|
           renderer.(node: node, position: position, tree: enumerable_tree)
