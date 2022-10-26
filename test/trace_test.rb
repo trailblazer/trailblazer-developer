f50d27fd4e44a06f66f07e02ce5f9542ae694765
Nick Sutterer <apotonick@gmail.com>
2022-10-26 10:31:03 +0200

    fix all trace_test things.

--- test/trace_test.rb
+++ test/trace_test.rb
@@ -23,7 +23,7 @@ class TraceTest < Minitest::Spec
     assert_equal ctx.inspect, %{{:seq=>[:b, :c]}}
     assert_equal flow_options[:flow].inspect, %{true}
 
-    output = Dev::Trace::Present.(stack, options_for_renderer: {label: {bc => bc.inspect}})
+    output = Dev::Trace::Present.(stack, label: {bc => bc.inspect})
     output = output.gsub(/0x\w+/, "").gsub(/0x\w+/, "").gsub(/@.+_test/, "")
 
     _(output).must_equal %{#<Trailblazer::Activity:>
@@ -46,7 +46,7 @@ class TraceTest < Minitest::Spec
 
     assert_equal ctx[:seq], [:a, :b, :c, :d, :e]
 
-    output = Dev::Trace::Present.(stack, options_for_renderer: {label: {activity => "#{activity.superclass} (anonymous)"}})
+    output = Dev::Trace::Present.(stack, label: {activity => "#{activity.superclass} (anonymous)"})
 
     puts output = output.gsub(/0x\w+/, "").gsub(/0x\w+/, "").gsub(/@.+_test/, "")
 
@@ -90,42 +90,41 @@ class TraceTest < Minitest::Spec
     assert_equal captured_output.data, { ctx: { seq: [:b, :c] }, signal: signal }
   end
 
-  it "Present allows to inject :renderer and pass through additional arguments to the renderer" do
-    stack, _ = Dev::Trace.invoke(
-      nested_activity,
-      [
-        { seq: [] },
-        {}
-      ]
-    )
+  it "Present allows to inject :renderer and pass through additional arguments to the renderer (e.g. {:color})" do
+    stack, _ = Dev::Trace.invoke(nested_activity, [{ seq: [] }, {}])
 
-    renderer = ->(task_node:, position:, tree:) do
-      assert_equal tree[position], task_node
-      task = task_node.input.task
+    renderer = ->(task_node:, tree:, color:, label:, **) do
+      task = task_node.captured_input.task
+
+      id = label[task] || Trailblazer::Activity::Introspect::Graph(task_node.captured_input.activity).find { |n| n.task == task }.id
+
       if task.is_a? Method
         task = "#<Method: Trailblazer::Activity::Testing::Assertions::Implementing.#{task.name}>"
       end
       [
         task_node.level,
-        %{#{task_node.level}/#{task}/#{task_node.output.data[:signal]}/#{task_node.value}/#{task_node.color}}
+        %{#{task_node.level}/#{task}/#{task_node.captured_output.data[:signal]}/#{id}/#{color}}
       ]
     end
 
-    output = Dev::Trace::Present.(stack, renderer: renderer,
-      color: "pink" # additional options.
+    output = Dev::Trace::Present.(
+      stack,
+      renderer: renderer,
+      color:    "pink", # additional options.
+      label: {nested_activity => nested_activity.inspect}
     )
 
     output = output.gsub(/0x\w+/, "").gsub(/0x\w+/, "").gsub(/@.+_test/, "")
 
-    _(output).must_equal %{`-- 1/#<Trailblazer::Activity:>/#<Trailblazer::Activity::End semantic=:success>/#<Trailblazer::Activity:>/pink
-    |-- 2/#<Trailblazer::Activity::Start semantic=:default>/Trailblazer::Activity::Right/Start.default/pink
-    |-- 2/#<Method: Trailblazer::Activity::Testing::Assertions::Implementing.b>/Trailblazer::Activity::Right/B/pink
-    |-- 2/#<Trailblazer::Activity:>/#<Trailblazer::Activity::End semantic=:success>/D/pink
-    |   |-- 3/#<Trailblazer::Activity::Start semantic=:default>/Trailblazer::Activity::Right/Start.default/pink
-    |   |-- 3/#<Method: Trailblazer::Activity::Testing::Assertions::Implementing.b>/Trailblazer::Activity::Right/B/pink
-    |   |-- 3/#<Method: Trailblazer::Activity::Testing::Assertions::Implementing.c>/Trailblazer::Activity::Right/C/pink
-    |   `-- 3/#<Trailblazer::Activity::End semantic=:success>/#<Trailblazer::Activity::End semantic=:success>/End.success/pink
-    |-- 2/#<Method: Trailblazer::Activity::Testing::Assertions::Implementing.f>/Trailblazer::Activity::Right/E/pink
-    `-- 2/#<Trailblazer::Activity::End semantic=:success>/#<Trailblazer::Activity::End semantic=:success>/End.success/pink}
+    _(output).must_equal %{0/#<Trailblazer::Activity:>/#<Trailblazer::Activity::End semantic=:success>/#<Trailblazer::Activity:>/pink
+|-- 1/#<Trailblazer::Activity::Start semantic=:default>/Trailblazer::Activity::Right/Start.default/pink
+|-- 1/#<Method: Trailblazer::Activity::Testing::Assertions::Implementing.b>/Trailblazer::Activity::Right/B/pink
+|-- 1/#<Trailblazer::Activity:>/#<Trailblazer::Activity::End semantic=:success>/D/pink
+|   |-- 2/#<Trailblazer::Activity::Start semantic=:default>/Trailblazer::Activity::Right/Start.default/pink
+|   |-- 2/#<Method: Trailblazer::Activity::Testing::Assertions::Implementing.b>/Trailblazer::Activity::Right/B/pink
+|   |-- 2/#<Method: Trailblazer::Activity::Testing::Assertions::Implementing.c>/Trailblazer::Activity::Right/C/pink
+|   `-- 2/#<Trailblazer::Activity::End semantic=:success>/#<Trailblazer::Activity::End semantic=:success>/End.success/pink
+|-- 1/#<Method: Trailblazer::Activity::Testing::Assertions::Implementing.f>/Trailblazer::Activity::Right/E/pink
+`-- 1/#<Trailblazer::Activity::End semantic=:success>/#<Trailblazer::Activity::End semantic=:success>/End.success/pink}
   end
 end
