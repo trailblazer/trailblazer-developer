2f58f2a40fe63aee8dae2063bf763f792e944687
Nick Sutterer <apotonick@gmail.com>
2023-04-27 16:44:43 +0200

    handle exception case in `#wtf?` more neatly. the stack is only completed when an exception occurred. the `:render_method` callable can now return output and additional returned values.

--- lib/trailblazer/developer/wtf.rb
+++ lib/trailblazer/developer/wtf.rb
@@ -16,39 +16,45 @@ module Trailblazer::Developer
     # This allows to display the trace even when an exception happened
     def invoke(activity, (ctx, flow_options), present_options: {}, **circuit_options)
       flow_options ||= {}
+      local_present_options = {}
 
       stack = Trace::Stack.new # unfortunately, we need this mutable object before things break.
+      raise_exception = false
 
-      complete_stack, signal, (ctx, flow_options) = Trace.invoke(
-        activity,
-        [ctx, flow_options.merge(stack: stack)],
-        **circuit_options
-      )
+      begin
+        complete_stack, signal, (ctx, flow_options) = Trace.invoke(
+          activity,
+          [ctx, flow_options.merge(stack: stack)],
+          **circuit_options
+        )
+      rescue
+        raise_exception = $! # TODO: will this show the very same stacktrace?
 
-      return signal, [ctx, flow_options], circuit_options
+        complete_stack, exception_source = Exception.complete_stack_for_exception(stack, $!)
 
-    ensure
-      # incomplete_stack = flow_options[:stack]
-      incomplete_stack = stack
+        local_present_options = {
+          # we can hand in options per node, identified by their captured_input part.
+          node_options: {
+            exception_source => {data: {exception_source: true}}, # goes to {Debugger::Node.build}
+          },
+          style: {exception_source => [:red, :bold]},
+        }
+      end
 
-        # in 99%, exception_source is a {Captured::Input}.
-      exception_source = incomplete_stack.to_a.last  # DISCUSS: in most cases, this is where the problem has happened.
-                                                #   However, what if an error happens in, say, an input filter? TODO: test this
-
-      complete_stack = Exception::Stack.complete(incomplete_stack) # TODO: only in case of exception!
-
-      puts Trace::Present.(
+      # always render the trace.
+      output, returned_args = Trace::Present.(
         complete_stack,
-        # we can hand in options per node, identified by their captured_input part.
-        node_options: {
-          exception_source => {data: {exception_source: true}}, # goes to {Debugger::Node.build}
-        },
-
         renderer:   Wtf::Renderer,
-        color_map:  Wtf::Renderer::DEFAULT_COLOR_MAP.merge( flow_options[:color_map] || {} ),
-        style: {exception_source => [:red, :bold]},
-        **present_options, # TODO: test.
+        color_map:  Wtf::Renderer::DEFAULT_COLOR_MAP.merge(flow_options[:color_map] || {}),
+        activity:   activity,
+        **local_present_options,
+        **present_options,
       )
+
+      puts output
+
+      raise raise_exception if raise_exception
+      return signal, [ctx, flow_options], circuit_options, output, returned_args
     end
 
     module Exception
@@ -72,6 +78,15 @@ module Trailblazer::Developer
         end
       end # Stack
 
+      def self.complete_stack_for_exception(incomplete_stack, exception)
+        # in 99%, exception_source is a {Captured::Input}.
+        exception_source = incomplete_stack.to_a.last  # DISCUSS: in most cases, this is where the problem has happened.
+                                                  #   However, what if an error happens in, say, an input filter? TODO: test this
+
+        complete_stack = Stack.complete(incomplete_stack)
+
+        return complete_stack, exception_source
+      end
     end
   end
 end
--- test/trace/wtf_test.rb
+++ test/trace/wtf_test.rb
@@ -40,15 +40,17 @@ class TraceWtfTest < Minitest::Spec
 
   it "traces until charlie, 3-level and exception occurs" do
     exception = nil
+    returned_args = nil
+
     output, _ = capture_io do
       exception = assert_raises RuntimeError do
-        Trailblazer::Developer.wtf?(alpha, [{ seq: Raiser.new(raise_in: :c) }])
+        returned_args = Trailblazer::Developer.wtf?(alpha, [{ seq: Raiser.new(raise_in: :c) }])
       end
     end
 
     assert_equal exception.message, %{hello from c!}
+    assert_nil returned_args
 
-puts output
     assert_equal output.gsub(/0x\w+/, ""), %{#<Class:>
 |-- \e[32mStart.default\e[0m
 |-- \e[32m#<Method: #<Class:>.a>\e[0m
@@ -62,11 +64,13 @@ puts output
   end
 
   it "traces until charlie, 3-level and step takes left track" do
+    returned_args = nil
+
     output, _ = capture_io do
-      Trailblazer::Developer.wtf?(alpha, [{ seq: [], c: false }])
+      returned_args = Trailblazer::Developer.wtf?(alpha, [{ seq: [], c: false }])
     end
 
-    _(output.gsub(/0x\w+/, "")).must_equal %{#<Class:>
+    trace_output = %(#<Class:>
 |-- \e[32mStart.default\e[0m
 |-- \e[32m#<Method: #<Class:>.a>\e[0m
 |-- #<Class:>
@@ -77,8 +81,18 @@ puts output
 |   |   |-- \e[33m#<Method: #<Class:>.c>\e[0m
 |   |   `-- End.failure
 |   `-- End.failure
-`-- End.failure
-}
+`-- End.failure)
+
+    # test returned values for #wtf?
+    assert_equal returned_args.size, 5
+    assert_equal returned_args[0].inspect, %(#<Trailblazer::Activity::End semantic=:failure>)
+    assert_equal returned_args[1][0].inspect, %({:seq=>[:a, :b, :c], :c=>false})
+    assert_equal returned_args[1][1].class, Hash # flow_options
+    assert_equal returned_args[2].inspect, %({}) # circuit_options
+    assert_equal returned_args[3].chomp, output.chomp # fourth returned value is the trace output.
+    assert_equal returned_args[4].inspect, %(nil)
+
+    assert_equal output.gsub(/0x\w+/, "").chomp, trace_output
   end
 
   it "traces alpha and it's subprocesses, for successful execution" do
@@ -86,7 +100,7 @@ puts output
       Trailblazer::Developer.wtf?(alpha, [{ seq: [] }])
     end
 
-    _(output.gsub(/0x\w+/, "")).must_equal %{#<Class:>
+    assert_equal output.gsub(/0x\w+/, ""), %(#<Class:>
 |-- \e[32mStart.default\e[0m
 |-- \e[32m#<Method: #<Class:>.a>\e[0m
 |-- #<Class:>
@@ -101,7 +115,41 @@ puts output
 |   `-- End.success
 |-- \e[32m#<Method: #<Class:>.aa>\e[0m
 `-- End.success
-}
+)
+  end
+
+  it "accepts {:present_options}" do
+    my_renderer = ->(debugger_nodes, **) {
+      return "Nodes: #{debugger_nodes.size}", ["additional", "returned", "args"]
+    }
+
+    signal, ctx, flow_options, circuit_options, output, returned_present_args = nil
+
+    captured_output, _ = capture_io do
+      signal, (ctx, flow_options), circuit_options, output, returned_present_args = Trailblazer::Developer.wtf?(alpha, [{seq: []}],
+        present_options: {render_method: my_renderer})
+    end
+
+    assert_equal captured_output.chomp, %(Nodes: 15)
+    assert_equal signal.inspect, %(#<Trailblazer::Activity::End semantic=:success>)
+    assert_equal ctx.inspect, %({:seq=>[:a, :b, :c, :cc, :bb, :aa]})
+    assert_equal circuit_options, {}
+    assert_equal output, captured_output.chomp
+    assert_equal returned_present_args, ["additional", "returned", "args"]
+  end
+
+  it "passes {activity} to {Present}" do
+    class PresentCreate < Trailblazer::Activity::Railway
+    end
+
+    my_renderer = ->(debugger_nodes, activity:, **) { "Nodes: #{debugger_nodes.size}, started at #{activity}" }
+
+    signal, (ctx, flow_options), circuit_options, output = Trailblazer::Developer.wtf?(
+      PresentCreate,
+      [{}],
+      present_options: {render_method: my_renderer})
+
+    assert_equal output, %(Nodes: 3, started at TraceWtfTest::PresentCreate)
   end
 
   it "overrides default color map of entities" do
