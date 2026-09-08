0e2dffb662c1f91f81e37ef516873a71f7ec63cd
Nick Sutterer <apotonick@gmail.com>
2026-09-08 10:11:58 +0200

    clean up Present. life can be so easy.

--- lib/trailblazer/developer/trace/present.rb
+++ lib/trailblazer/developer/trace/present.rb
@@ -29,25 +29,6 @@ module Trailblazer::Developer
 
         return render_method.(trace: trace_nodes)
       end
-
-      def deprecate_node_options!(node_options: nil, **) # TODO: remove in 0.2.0.
-        return unless node_options
-
-        raise "[Trailblazer] The `:node_options` option for `Trace::Present` is deprecated.
-  Please use the block style as described here: https://trailblazer.to/2.1/docs/internals.html#internals-developer-trace-present"
-      end
-
-      # @private
-      def merge_local_options(options, local_options)
-        merged_hash = options.collect do |key, value|
-          [
-            key,
-            value.is_a?(Hash) ? local_options.fetch(key, {}).merge(value) : value # options are winning over local_options[key]
-          ]
-        end.to_h
-
-        local_options.merge(merged_hash)
-      end
     end # Present
   end
 end
